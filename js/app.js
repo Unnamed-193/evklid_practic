73c28b7f57dc4e035755f4dd40df5da99566abcf
Iksur <milan.romodin@mail.ru>
2023-10-08 11:36:18 +0300

    ошибка секця work

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay:{
+    delay:3000,
+  },
+  allowTouchMove:true,
+  loop:true,
+});
+
