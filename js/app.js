04d12662f4e856400fce7b267eb65014eee32bb6
Iksur <milan.romodin@mail.ru>
2023-10-29 11:45:37 +0300

    доделал сайт

--- js/app.js
+++ js/app.js
@@ -10,3 +10,71 @@ var swiper = new Swiper(".swiper", {
   loop:true,
 });
 
+const navList = document.querySelector (".nav__list")
+const navBurger = document.querySelector (".nav__burger")
+navBurger.addEventListener ("click", ()=>{
+  navList.classList.toggle("active")
+  navBurger.classList.toggle("active")
+})
+
+// tabs
+
+
+const tabsBtn = document.querySelectorAll (".work__btn")
+const tabsItem = document.querySelectorAll (".tab-content")
+tabsBtn.forEach(clickTab)
+function clickTab (item){
+  item.addEventListener("click", function(){
+    let currentBtn = item
+    let tabId = currentBtn.getAttribute ("data-tab")
+    let currentTab = document.querySelector (tabId)
+
+
+    if (!currentBtn.classList.contains("active")){
+      tabsBtn.forEach(function(btn){
+        btn.classList.remove("active")
+      })
+
+      tabsItem.forEach(function(item){
+        item.classList.remove("active")
+      })
+
+      currentBtn.classList.add("active")
+      currentTab.classList.add("active")
+
+    }
+
+  })
+}
+
+document.querySelector (".work__btn").click()
+
+const search = document.querySelector(".search")
+const searchBtn = document.querySelector(".header__btn")
+const clear = document.querySelector(".search__clear")
+searchBtn.addEventListener ('click',()=>{
+  search.classList.add('open')
+  searchBtn.classList.add('open')
+  clear.classList.add('open')
+})
+
+
+
+clear.addEventListener ('click',()=>{
+  search.classList.remove('open')
+  searchBtn.classList.remove('open')
+  clear.classList.remove('open')
+  document.getElementById('input').value = ''
+})
+
+// accordion
+
+const accordion = document.querySelectorAll ('[data-collapse]')
+accordion.forEach ((question,index) => {
+  accordion [index].addEventListener('click',() => {
+    question.classList.toggle("active")
+  })
+})
+
+
+
